1b2e5e502e2db3d97f35542feff8f04aa8a1d125
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2020-06-05 10:23:55 -0300

    Projeto finalizado da semana Next Level Week #01

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import cors from 'cors';
+import express from 'express';
+import path from 'path';
+import { errors } from 'celebrate';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/api/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
+
+app.listen(3333);
